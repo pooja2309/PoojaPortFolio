98ca7b41cb19ed8dde2dfc8ea096e99549e5e782
pooja2309 <pooja.adi2309@gmail.com>
2025-09-19 10:57:15 -0600

    Updated profile picture

--- client/src/pages/home.tsx
+++ client/src/pages/home.tsx
@@ -752,7 +752,7 @@ export default function Home() {
               </h1>
 
               <img
-                src="https://media.licdn.com/dms/image/v2/D5603AQGl8_CkvubZMQ/profile-displayphoto-crop_800_800/B56ZhtbyCrH0AQ-/0/1754182658173?e=1758153600&v=beta&t=pl8zujEhanFhE81XTH_z7VRqnmc3qE_95NCXe9g9Dz4"
+                src="/src/images/pooja-suresh-headshot.jpeg"
                 alt="Pooja Suresh professional headshot"
                 className="w-40 h-40 sm:w-48 sm:h-48 md:w-56 md:h-56 lg:w-64 lg:h-64 rounded-full border-4 border-white/20 shadow-xl object-cover"
               />
